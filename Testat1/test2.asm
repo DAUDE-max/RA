91437937a9e158ea2563f33cd23c2dbb16faffa4
daude <mas7726@thi.de>
2025-11-07 14:32:46 +0100

    t

---
+++ Testat1/test2.asm
@@ -0,0 +1,47 @@
+; Schmid Maximilian, Axel Soler García
+;---------------------------------------------------------------
+; Lab1.asm
+; 
+;
+; Author: NSFR
+;
+; ---------------------------------------------------------------
+
+; declaration of functions that are not defined in the module being assembled, i.e. this file
+
+extern printf; Using the  c function for output - the object file needs to be linked with a C library
+
+section .data ; defines which section of the output file the code will be assembled into
+              ; here: initialised data
+              ; segment can be used instead (exactly equivalent synonym)
+
+  decformat: db `%d \n`,0 ; we define the decimal format for use with C's printf function
+  n: dd 20
+
+section .text    ;code segment starts here
+
+global main
+
+main:
+      push ebp
+      mov ebp, esp
+
+      mov ecx, 0 ;Counter
+      mov edx, 0 ;Storage
+      mov eax, 0 ;var1
+      mov ebx, 1 ;var2
+
+next:
+      push edx
+      push decformat
+      inc ecx
+      call printf
+      
+      cmp ecx, [n] 
+      jne next
+
+      add esp, 8
+      mov eax,0      ; returning 0 by convention
+      mov esp, ebp   ; clean up after ourselves 
+      pop ebp        ; leave could be used alternatively for the last two commands
+   ret
