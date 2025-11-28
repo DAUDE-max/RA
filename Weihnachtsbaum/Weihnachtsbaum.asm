27cf703f062153383fa315e5cd012f581dff84b8
NotNewton <185932133+NotNewton01@users.noreply.github.com>
2025-11-28 08:39:28 +0100

    Fixed Makefile and started Weihnachtsbaum

---
+++ Weihnachtsbaum/Weihnachtsbaum.asm
@@ -0,0 +1,72 @@
+;Programm von Max Schmidt und Axel Soler García
+
+extern printf
+extern atoi
+
+section .data
+	decformat: db `%d \n`,0
+        Fehler: db `Ein Fehler ist aufgetreten!\n`,0
+section .text
+
+global main
+main:
+	push ebp
+	mov ebp, esp
+
+; Check if 1 arg
+	mov eax, [ebp+8]
+	cmp eax, 2
+	jne error
+
+	; Check arg > 0
+	mov ebx, [ebp +12]
+	push dword[ebx+4]
+	call atoi
+	add esp, 4
+	mov ebx, eax
+	cmp ebx, 0
+	jng error
+
+arbeit:
+	push ebx
+	push decformat
+	call printf
+	add esp, 4
+
+	pop ebx
+	cmp ebx, 1
+	je ende
+
+	mov eax, ebx
+	cdq
+	mov ecx, 2
+	idiv ecx
+	cmp edx, 0
+	je gerade
+	jmp ungerade
+
+gerade:
+	;fall gerade
+	mov ebx, eax
+	jmp arbeit
+
+ungerade:
+	;fall unegarde
+	mov eax, ebx
+	mov ecx, 3
+	imul eax, ecx
+	add eax, 1
+	mov ebx, eax
+	jmp arbeit
+
+error:
+        push Fehler
+        call printf
+
+ende:
+	add esp, 4
+	mov esp, ebp
+	pop ebp
+ret
+
+
